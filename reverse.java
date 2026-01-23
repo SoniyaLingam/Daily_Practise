056a77bcfffd8cce1cece7534ebd50fb7e006c24
SoniyaLingam <sonili2404@gmail.com>
2026-01-24 00:10:20 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class reverse {
+    public static void main (String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int u=0;u<n;u++){
+            arr[u]=sc.nextInt();
+        }
+        int l=1;
+        int r=(n%2==0)?n-1:n-2;
+        while(l<r){
+            int t=arr[l];
+            arr[l]=arr[r];
+            arr[r]=t;
+            l+=2;
+            r-=2;
+        }
+        for(int u=0;u<n;u++){
+            System.out.print(arr[u]);
+        }
+    }
+    
+}
