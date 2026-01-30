55114742f22db64524421a049074a09e2f7def72
SoniyaLingam <sonili2404@gmail.com>
2026-01-30 23:02:49 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -1,24 +1,18 @@
 import java.util.Scanner;
-public class reverse {
-    public static void main (String[] args){
+public class reverse{
+    public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] arr=new int[n];
-        for(int u=0;u<n;u++){
-            arr[u]=sc.nextInt();
+        String a=sc.nextLine();
+        String[] w=a.trim().split("\\s+");
+        StringBuilder r=new StringBuilder();
+        int n=w.length;
+        for(int u=n-1;u>=0;u--){
+            r.append(w[u]);
+            if(u!=0)r.append(" ");
+
         }
-        int l=1;
-        int r=(n%2==0)?n-1:n-2;
-        while(l<r){
-            int t=arr[l];
-            arr[l]=arr[r];
-            arr[r]=t;
-            l+=2;
-            r-=2;
-        }
-        for(int u=0;u<n;u++){
-            System.out.print(arr[u]);
-        }
+        System.out.println(r.toString());
+
     }
-    
 }
+
