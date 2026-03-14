f761f2ee3abc67df26453ce3bc6bcb1c78189b08
SoniyaLingam <sonili2404@gmail.com>
2026-03-14 23:45:38 +0530

    Add files via upload

--- pattern.java
+++ pattern.java
@@ -1,21 +1,23 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 import java.util.*;
-public class pattern{
+public class pattern {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         for(int u=0;u<n;u++){
-            for(int s=0;s<n-u-1;s++){
-                System.out.print(" ");
+            for(int j=0;j<n;j++){
+                if(j==0 || j==n-1){
+                    System.out.print("*");
+                }
+                
+                else if(u==j){
+                    System.out.print("*");
+                }else{
+                    System.out.print("-");
+                }
             }
-            for(int j=0;j<2*u+1;j++){
-                System.out.print("*");
-            }
-             for(int s=0;s<n-u-1;s++){
-                System.out.print(" ");
-            }  
-        
-        System.out.println();
+            System.out.println();
         }
     }
 }
-
