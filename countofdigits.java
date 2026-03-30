654939ec313ec6d0b81eb33e9254ee07bcfd3d48
SoniyaLingam <sonili2404@gmail.com>
2026-03-30 23:20:17 +0530

    Add files via upload

---
+++ countofdigits.java
@@ -0,0 +1,16 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+
+public class countofdigits  {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int c=0;
+        while(n>0){
+            c++;
+            n/=10;
+        }
+        System.out.print(c);
+    }
+}
