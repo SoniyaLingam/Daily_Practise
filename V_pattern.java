5f3e3fe5dc842804cce2422a4760b1c8fc1573f0
Soniya Lingam M <sonili2404@gmail.com>
2026-08-16 16:46:23 +0530

    Add files via upload

--- V_pattern.java
+++ V_pattern.java
@@ -6,24 +6,27 @@ Write your code in this editor and press "Run" button to execute it.
 
 *******************************************************************************/
 import java.util.*;
-public class flowchart
+public class V_pattern
 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int num;
-		int sum=0;
-		while(true){
-		    num=sc.nextInt();
-		    if(num%2==0){
-		        sum+=num;
+		int n=sc.nextInt();
+		int q=(n/2)+1;
+		int k=1;
+		int s=n;
+		for(int u=0;u<q;u++){
+		    for(int j=0;j<n;j++){
+		        if(u==j){
+		            System.out.print(k);
+		            k++;
+		        }else if(u==n-j-1){
+		            System.out.print(s);
+		            s--;
+		        }else{
+		            System.out.print("*");
+		        }
 		    }
-		    if(num!=10){
-		         continue;
-		    }else{
-		        System.out.print(sum);
-		        break;
-		    }
-		    
+			System.out.println();
 		}
 	}
 }
